d6427f0723f1f60e9a21501f36c79c956805a176
fossilfriend <egreenfest@gmail.com>
2025-01-09 14:26:24 -0500

    add comment for genomicsdb rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+
+    /* for accessing GenomicsDB services and static files; e.g., genome browser tracks
+    async rewrites() {
+        return [
+            {
+                source: '/service/:path*',
+                destination: 'https://www.niagads.org/genomics/service/:path*'
+            },
+            {
+                source: '/files/:path*',
+                destination: 'https://www.niagads.org/genomics/files/:path*'
+            }
+        ]
+    }
+    */
 };
 
 export default nextConfig;
