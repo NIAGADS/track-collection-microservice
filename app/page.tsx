6990789f0c1eb8a02c9e8674e9b01a66ad7317be
fossilfriend <egreenfest@gmail.com>
2025-02-21 03:37:55 -0500

    working page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
-"use client"
-import React, { cache, useEffect, useMemo, useState } from "react"
-import Table, {TableProps} from "@bug_sam/table";
+"use client";
+import React, { cache, Suspense, useEffect, useMemo, useState } from "react";
+import Table from "@bug_sam/table";
 import { Collection } from "@/common/types";
+import { Card, Skeleton } from "@heroui/react";
 
-type RESPONSE_TYPE = 'collection-listing' | 'track-listing'
+type RESPONSE_TYPE = "collection-listing" | "track-listing";
 
-// http://localhost:8000/filer/collection/?xqtl-project
 const fetchCollectionInformation = cache(
     async (collection: Collection, responseType: RESPONSE_TYPE) => {
-        const queryString = responseType === 'collection-listing' ? `?${collection.name}` : `/${collection.name}?view=table`
+        const queryString =
+            responseType === "collection-listing"
+                ? `?${collection.name}`
+                : `/${collection.name}?view=table`;
         const requestUrl = `/api/${collection.route}/collection${queryString}`;
         let data = null;
         try {
@@ -29,28 +32,65 @@ const fetchCollectionInformation = cache(
 );
 
 // TODO: error handling
-export const fetchCollectionMetadata = cache(
-    async (collection: Collection) => {
-        const response: any = await fetchCollectionInformation(collection, 'collection-listing' )
-        const match =  response.find((c: any) => c.name.toLowerCase() == collection.name.toLowerCase());
-        Object.assign(collection, {label: match.name, description: match.description})
-        return collection
-    }
-);
+export const fetchCollectionMetadata = cache(async (collection: Collection) => {
+    const response: any = await fetchCollectionInformation(
+        collection,
+        "collection-listing"
+    );
+    const match = response.find(
+        (c: any) => c.name.toLowerCase() == collection.name.toLowerCase()
+    );
+    Object.assign(collection, {
+        label: match.name,
+        description: match.description,
+    });
+    return collection;
+});
 
+interface Row {
+    [k: string]: any;
+}
+interface TableProps {
+    id: string;
+    data: Row[];
+    columns: any;
+    options: any;
+}
 
+function LoadingTable() {
+    return (
+        <div className="space-y-6">
+            <div className="space-y-2">
+                <Skeleton className="w-[30ch] h-[1.25rem]" />
+                <Skeleton className="w-[45ch] h-[1rem]" />
+            </div>
+            <div className="space-y-2">
+                <Skeleton className="w-[30ch] h-[1.25rem]" />
+                <Skeleton className="w-[45ch] h-[1rem]" />
+            </div>
+            <div className="space-y-2">
+                <Skeleton className="w-[30ch] h-[1.25rem]" />
+                <Skeleton className="w-[45ch] h-[1rem]" />
+            </div>
+        </div>
+    );
+}
 
 export default function Home() {
     const [loading, setLoading] = useState<boolean>(true);
-    const [collection, setCollection ] = useState<Collection|null>(null);
-    const [trackTable, setTrackTable] = useState<TableProps|null>(null);
-    
+    const [collection, setCollection] = useState<Collection | null>(null);
+    const [trackTable, setTrackTable] = useState<TableProps | null>(null);
+    const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
     useEffect(() => {
         try {
             console.log(process.env.NEXT_PUBLIC_TRACK_COLLECTION);
-            const [route, name] = process.env
-                .NEXT_PUBLIC_TRACK_COLLECTION!.split(":");
-            fetchCollectionMetadata( { route: route, name: name } as Collection).then((result) => setCollection(result))
+            const [route, name] =
+                process.env.NEXT_PUBLIC_TRACK_COLLECTION!.split(":");
+            fetchCollectionMetadata({
+                route: route,
+                name: name,
+            } as Collection).then((result) => setCollection(result));
         } catch (err) {
             console.error(
                 "Error parsing track collections; NEXT_PUBLIC_TRACK_COLLECTION (set in application .env file);   an API route:collection_name pair."
@@ -58,25 +98,45 @@ export default function Home() {
         }
     }, []);
 
-    
     useEffect(() => {
-        collection && fetchCollectionInformation(collection, 'track-listing').then((result) => setTrackTable(result))
-    }, [collection])
+        collection &&
+            fetchCollectionInformation(collection, "track-listing").then(
+                (result) => setTrackTable(result)
+            );
+    }, [collection]);
 
-    // or useMemo?
-    //{ type: "link", url: string, tooltip?: string }>>
-    const tableData = useMemo(() => {
+    useEffect(() => {
         if (trackTable) {
-            const trackPkColIndex:number = trackTable.columns.findIndex((c: any) => c.id == process.env.NEXT_PUBLIC_TRACK_PK_FIELD)
-            return trackTable.data.map((row: any) => row.map((cell: any, index: number) => (
-                index == trackPkColIndex ? cell : Object.assign(cell, {type: 'link', 'url': `/track/${cell.value}`}))));
+            for (const c of trackTable.columns) {
+                if (c.id == "track_id") {
+                    Object.assign(c, { type: "link" });
+                }
+            }
+            for (const t of trackTable.data) {
+                Object.assign(t, {
+                    track_id: {
+                        value: t.track_id,
+                        type: "link",
+                        url: `/track/${t.track_id}`,
+                        tooltip: "get track data",
+                    },
+                });
+            }
+            setIsLoaded(true);
         }
-        else {
-            return null;
-        }
-    }, [trackTable?.data])
+    }, [trackTable]);
 
-    
-    return tableData && <Table data={tableData} columns={trackTable!.columns} id={trackTable!.id} options={trackTable!.options}/>
-
+    return (
+        <Card className="w-[200px] space-y-5 p-4" radius="lg">
+            <Suspense
+                fallback={<LoadingTable/>}>
+                <Table
+                    data={trackTable!.data}
+                    columns={trackTable!.columns}
+                    id={trackTable!.id}
+                    options={trackTable!.options}
+                />
+            </Suspense>
+        </Card>
+    );
 }
